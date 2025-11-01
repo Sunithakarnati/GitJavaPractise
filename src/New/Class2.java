1b6a7dd196cdcb914af05d614924d089e3ea395b
SunithaKarnati <sunithakarnati546@gmail.com>
2025-11-01 16:45:28 +0530

    all files are added now

---
+++ src/New/Class2.java
@@ -0,0 +1,12 @@
+package New;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+		
+		System.out.println("Heloo welcom to java");
+
+	}
+
+}
